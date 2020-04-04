c0147903ed7ec1dc1bc584e6518463411bceceb6
I3OR2A <ioariroi@gmail.com>
2020-04-04 16:40:56 +0800

    7-5 关键词索引对象定义与服务实现

---
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/utils/CommonUtils.java
@@ -0,0 +1,24 @@
+package com.imooc.ad.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.time.DateUtils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+import java.util.function.Supplier;
+
+/**
+ * Created by Qinyi.
+ */
+@Slf4j
+public class CommonUtils {
+
+    public static <K, V> V getorCreate(K key, Map<K, V> map,
+                                       Supplier<V> factory) {
+        return map.computeIfAbsent(key, k -> factory.get());
+    }
+}
