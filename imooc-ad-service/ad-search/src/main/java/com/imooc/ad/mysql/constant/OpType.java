2fda0039198406aaa6ca81f3b8a0309ed071089a
I3OR2A <ioariroi@gmail.com>
2020-04-05 00:16:45 +0800

    8-6 【在理解的基础上编码】索引操作Handler的定义与说明

---
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/mysql/constant/OpType.java
@@ -0,0 +1,15 @@
+package com.imooc.ad.mysql.constant;
+
+import com.github.shyiko.mysql.binlog.event.EventType;
+
+/**
+ * Created by Qinyi.
+ */
+public enum OpType {
+
+    ADD,
+    UPDATE,
+    DELETE,
+    OTHER;
+
+}
