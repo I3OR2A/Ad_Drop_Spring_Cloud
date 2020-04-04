f018b892147c9687ed5e8680cdca8561e8a36a0a
I3OR2A <ioariroi@gmail.com>
2020-04-05 00:00:32 +0800

    8-3 表数据导出到文件功能实现（1）

---
+++ imooc-ad-service/ad-sponsor/src/test/java/com/imooc/ad/service/DumpDataService.java
@@ -0,0 +1,67 @@
+package com.imooc.ad.service;
+
+import com.alibaba.fastjson.JSON;
+import com.imooc.ad.Application;
+import com.imooc.ad.constant.CommonStatus;
+import com.imooc.ad.dao.AdPlanRepository;
+import com.imooc.ad.dao.AdUnitRepository;
+import com.imooc.ad.dao.CreativeRepository;
+import com.imooc.ad.dao.unit_condition.AdUnitDistrictRepository;
+import com.imooc.ad.dao.unit_condition.AdUnitItRepository;
+import com.imooc.ad.dao.unit_condition.AdUnitKeywordRepository;
+import com.imooc.ad.dao.unit_condition.CreativeUnitRepository;
+import com.imooc.ad.dump.DConstant;
+import com.imooc.ad.dump.table.AdCreativeTable;
+import com.imooc.ad.dump.table.AdCreativeUnitTable;
+import com.imooc.ad.dump.table.AdPlanTable;
+import com.imooc.ad.dump.table.AdUnitDistrictTable;
+import com.imooc.ad.dump.table.AdUnitItTable;
+import com.imooc.ad.dump.table.AdUnitKeywordTable;
+import com.imooc.ad.dump.table.AdUnitTable;
+import com.imooc.ad.entity.AdPlan;
+import com.imooc.ad.entity.AdUnit;
+import com.imooc.ad.entity.Creative;
+import com.imooc.ad.entity.unit_condition.AdUnitDistrict;
+import com.imooc.ad.entity.unit_condition.AdUnitIt;
+import com.imooc.ad.entity.unit_condition.AdUnitKeyword;
+import com.imooc.ad.entity.unit_condition.CreativeUnit;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Qinyi.
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Application.class},
+        webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class DumpDataService {
+
+    @Autowired
+    private AdPlanRepository planRepository;
+    @Autowired
+    private AdUnitRepository unitRepository;
+    @Autowired
+    private CreativeRepository creativeRepository;
+    @Autowired
+    private CreativeUnitRepository creativeUnitRepository;
+    @Autowired
+    private AdUnitDistrictRepository districtRepository;
+    @Autowired
+    private AdUnitItRepository itRepository;
+    @Autowired
+    private AdUnitKeywordRepository keywordRepository;
+}
