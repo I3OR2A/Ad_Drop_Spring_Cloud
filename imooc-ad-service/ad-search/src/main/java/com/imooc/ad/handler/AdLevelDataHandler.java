c33c66f7ccf5e0f3f451dba75d45ea2ffd94b8c2
I3OR2A <ioariroi@gmail.com>
2020-04-05 11:26:57 +0800

    8-7 第二层级索引操作的实现

--- imooc-ad-service/ad-search/src/main/java/com/imooc/ad/handler/AdLevelDataHandler.java
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/handler/AdLevelDataHandler.java
@@ -37,6 +37,44 @@ import java.util.Set;
 @Slf4j
 public class AdLevelDataHandler {
 
+    public static void handleLevel2(AdPlanTable planTable, OpType type) {
+
+        AdPlanObject planObject = new AdPlanObject(
+                planTable.getId(),
+                planTable.getUserId(),
+                planTable.getPlanStatus(),
+                planTable.getStartDate(),
+                planTable.getEndDate()
+        );
+        handleBinlogEvent(
+                DataTable.of(AdPlanIndex.class),
+                planObject.getPlanId(),
+                planObject,
+                type
+        );
+    }
+
+    public static void handleLevel2(AdCreativeTable creativeTable,
+                                    OpType type) {
+
+        CreativeObject creativeObject = new CreativeObject(
+                creativeTable.getAdId(),
+                creativeTable.getName(),
+                creativeTable.getType(),
+                creativeTable.getMaterialType(),
+                creativeTable.getHeight(),
+                creativeTable.getWidth(),
+                creativeTable.getAuditStatus(),
+                creativeTable.getAdUrl()
+        );
+        handleBinlogEvent(
+                DataTable.of(CreativeIndex.class),
+                creativeObject.getAdId(),
+                creativeObject,
+                type
+        );
+    }
+
     private static <K, V> void handleBinlogEvent(
             IndexAware<K, V> index,
             K key,
