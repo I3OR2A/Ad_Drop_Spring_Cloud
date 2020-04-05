de088fd6abe75050616cdd2e6a1fc069f1603268
I3OR2A <ioariroi@gmail.com>
2020-04-05 11:36:02 +0800

    8-8 第三层级索引操作的实现

--- imooc-ad-service/ad-search/src/main/java/com/imooc/ad/handler/AdLevelDataHandler.java
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/handler/AdLevelDataHandler.java
@@ -75,6 +75,69 @@ public class AdLevelDataHandler {
         );
     }
 
+    public static void handleLevel3(AdUnitTable unitTable, OpType type) {
+
+        AdPlanObject adPlanObject = DataTable.of(
+                AdPlanIndex.class
+        ).get(unitTable.getPlanId());
+        if (null == adPlanObject) {
+            log.error("handleLevel3 found AdPlanObject error: {}",
+                    unitTable.getPlanId());
+            return;
+        }
+
+        AdUnitObject unitObject = new AdUnitObject(
+                unitTable.getUnitId(),
+                unitTable.getUnitStatus(),
+                unitTable.getPositionType(),
+                unitTable.getPlanId(),
+                adPlanObject
+        );
+
+        handleBinlogEvent(
+                DataTable.of(AdUnitIndex.class),
+                unitTable.getUnitId(),
+                unitObject,
+                type
+        );
+    }
+
+    public static void handleLevel3(AdCreativeUnitTable creativeUnitTable,
+                                    OpType type) {
+
+        if (type == OpType.UPDATE) {
+            log.error("CreativeUnitIndex not support update");
+            return;
+        }
+
+        AdUnitObject unitObject = DataTable.of(
+                AdUnitIndex.class
+        ).get(creativeUnitTable.getUnitId());
+        CreativeObject creativeObject = DataTable.of(
+                CreativeIndex.class
+        ).get(creativeUnitTable.getAdId());
+
+        if (null == unitObject || null == creativeObject) {
+            log.error("AdCreativeUnitTable index error: {}",
+                    JSON.toJSONString(creativeUnitTable));
+            return;
+        }
+
+        CreativeUnitObject creativeUnitObject = new CreativeUnitObject(
+                creativeUnitTable.getAdId(),
+                creativeUnitTable.getUnitId()
+        );
+        handleBinlogEvent(
+                DataTable.of(CreativeUnitIndex.class),
+                CommonUtils.stringConcat(
+                        creativeUnitObject.getAdId().toString(),
+                        creativeUnitObject.getUnitId().toString()
+                ),
+                creativeUnitObject,
+                type
+        );
+    }
+
     private static <K, V> void handleBinlogEvent(
             IndexAware<K, V> index,
             K key,
