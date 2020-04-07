d0890676edf6d85ad6245f4bd309fcfcdb371991
dev <dev@example.com>
2020-04-07 23:37:57 +0800

    10-2 【跟我一起继续编码】第二层级增量数据的投递

--- imooc-ad-service/ad-search/src/main/java/com/imooc/ad/mysql/constant/OpType.java
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/mysql/constant/OpType.java
@@ -12,4 +12,18 @@ public enum OpType {
     DELETE,
     OTHER;
 
+    public static OpType to(EventType eventType) {
+
+        switch (eventType) {
+            case EXT_WRITE_ROWS:
+                return ADD;
+            case EXT_UPDATE_ROWS:
+                return UPDATE;
+            case EXT_DELETE_ROWS:
+                return DELETE;
+
+            default:
+                return OTHER;
+        }
+    }
 }
